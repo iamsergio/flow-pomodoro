d4b667c689afc4c43fe18dd46306eb38295519eb
Sergio Martins <iamsergio@gmail.com>
2014-08-26 11:44:33 +0100

    blackberry: Added playbook scripts

---
+++ build-playbook.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir build_dir-playbook &> /dev/null
+cd build_dir-playbook
+qmake ../flow.pro
+make -j8
+
+# blackberry-nativepackager -devMode -package flow-pomodoro.bar bar-descriptor.xml flow
+# blackberry-deploy -installApp -device 192.168.1.143 -password kdab flow-pomodoro.bar
