9da03b8e169f2b390a97d410a4bf733bbdb6196f
Sergio Martins <iamsergio@gmail.com>
2014-08-28 01:19:10 +0100

    blackberry: Fix the bar command

--- build-playbook.sh
+++ build-playbook.sh
@@ -5,5 +5,5 @@ cd build_dir-playbook
 qmake ../flow.pro
 make -j8
 
-# blackberry-nativepackager -devMode -package flow-pomodoro.bar bar-descriptor.xml flow
+# blackberry-nativepackager -devMode -package flow-pomodoro.bar bar-descriptor.xml
 # blackberry-deploy -installApp -device 192.168.1.143 -password kdab flow-pomodoro.bar
