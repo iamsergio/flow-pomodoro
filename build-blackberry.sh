dd92244c9e6238fc926338767eae40c803de8856
Sergio Martins <iamsergio@gmail.com>
2014-09-05 18:03:31 +0100

    Blackberry builds scripts

---
+++ build-blackberry.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir build_dir-blackberry &> /dev/null
+cd build_dir-blackberry
+qmake ../flow.pro
+make -j8
+
+echo "Making package..."
+cp ../bar-descriptor-blackberry.xml .
+blackberry-nativepackager -devMode -package flow-pomodoro.bar bar-descriptor-blackberry.xml
+
+echo "installing..."
+blackberry-deploy -installApp -device z10 -password 12345 flow-pomodoro.bar
