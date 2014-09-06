07049cf313872f1c07ddfcd1c04733272bff2127
Sergio Martins <iamsergio@gmail.com>
2014-09-06 20:51:02 +0100

    Validate qrc and OTHER_FILES

--- src/validate_qrc.sh
+++ src/validate_qrc.sh
@@ -1,6 +1,13 @@
 for i in qml/*.qml; do
     if ! grep $i resources.qrc &> /dev/null ; then
-        echo $i not in resources.qrc
+        echo "error:" $i not in resources.qrc
+        exit 1
+    fi
+done
+
+for i in qml/*.qml; do
+    if ! grep $i src.pro &> /dev/null ; then
+        echo "error:" $i not in src.pro
         exit 1
     fi
 done
