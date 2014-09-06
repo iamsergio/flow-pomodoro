dd6961579fdf548c4da5f159c0bf997cee669619
Sergio Martins <iamsergio@gmail.com>
2014-09-06 16:24:18 +0100

    Fix detecting success in unit-tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,20 +14,24 @@ int main(int argc, char *argv[])
     bool success = true;
 
     TestStorage test1;
-    success &= QTest::qExec(&test1, argc, argv);
+    success &= QTest::qExec(&test1, argc, argv) == 0;
 
     TestTask test2;
-    success &= QTest::qExec(&test2, argc, argv);
+    success &= QTest::qExec(&test2, argc, argv) == 0;
 
     TestTag test3;
-    success &= QTest::qExec(&test3, argc, argv);
+    success &= QTest::qExec(&test3, argc, argv) == 0;
 
     TestTagModel test4;
-    success &= QTest::qExec(&test4, argc, argv);
+    success &= QTest::qExec(&test4, argc, argv) == 0;
 
     TestCheckableTagModel test5;
-    success &= QTest::qExec(&test5, argc, argv);
+    success &= QTest::qExec(&test5, argc, argv) == 0;
 
     delete Kernel::instance(); // Otherwise it's deleted by QGuiApplication and we hang deep in qml engine land for some reason
+
+    if (!success)
+        qWarning() << "\nSome tests failed!!!";
+
     return success;
 }
