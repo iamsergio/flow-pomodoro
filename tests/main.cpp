9b8abee128c16e5bbeb86ca94b48a853517336bf
Sergio Martins <iamsergio@gmail.com>
2014-09-28 16:44:03 +0100

    unit-tests: Destroy each Kernel before starting a new test.
    So they don't interfere with each other.

--- tests/main.cpp
+++ tests/main.cpp
@@ -17,41 +17,59 @@ int main(int argc, char *argv[])
     app.setAttribute(Qt::AA_Use96Dpi, true);
     bool success = true;
 
-    TestStorage test1;
-    success &= QTest::qExec(&test1, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestStorage test1;
+        success &= QTest::qExec(&test1, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestTask test2;
-    success &= QTest::qExec(&test2, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestTask test2;
+        success &= QTest::qExec(&test2, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestTag test3;
-    success &= QTest::qExec(&test3, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestTag test3;
+        success &= QTest::qExec(&test3, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestTagModel test4;
-    success &= QTest::qExec(&test4, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestTagModel test4;
+        success &= QTest::qExec(&test4, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestCheckableTagModel test5;
-    success &= QTest::qExec(&test5, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestCheckableTagModel test5;
+        success &= QTest::qExec(&test5, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestArchivedTasksModel test6;
-    success &= QTest::qExec(&test6, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestArchivedTasksModel test6;
+        success &= QTest::qExec(&test6, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestStagedTasksModel test7;
-    success &= QTest::qExec(&test7, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestStagedTasksModel test7;
+        success &= QTest::qExec(&test7, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestTaskFilterModel test8;
-    success &= QTest::qExec(&test8, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestTaskFilterModel test8;
+        success &= QTest::qExec(&test8, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
-    TestWebDav test9;
-    success &= QTest::qExec(&test9, argc, argv) == 0;
-    Q_ASSERT(success);
+    {
+        TestWebDav test9;
+        success &= QTest::qExec(&test9, argc, argv) == 0;
+        Q_ASSERT(success);
+    }
 
     if (success)
         qDebug() << "Success!";
