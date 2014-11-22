11d8ec838f944614b80fe29cff4345df5b31d32e
Sergio Martins <sergio.martins@kdab.com>
2014-11-22 16:21:02 +0000

    Add more version options

--- src/generate_version.sh
+++ src/generate_version.sh
@@ -2,20 +2,27 @@
 
 function isTag()
 {
-# Chec
+    # Checks if HEAD is a tag
+    return git describe --exact-match &> /dev/null
 }
 
 
 cp version_template.h version.h
 LAST_SHA1=`git rev-parse HEAD`
 LAST_DATE=`git log -1 --format=%cd --date=short`
-LAST_TAG='git describe --abbrev=0'
+LAST_TAG=`git describe --abbrev=0`
 BRANCH=`git symbolic-ref --short HEAD`
 
-if ! git describe --exact-match &> /dev/null; then
-    echo exists
+if isTag ; then
+    ISTAG=true
+else
+    ISTAG=false
 fi
 
 
 sed -i "s/SHA1_REPLACEME/$LAST_SHA1/" version.h
 sed -i "s/DATE_REPLACEME/$LAST_DATE/" version.h
+
+sed -i "s/LASTTAG_REPLACEME/$LAST_TAG/" version.h
+sed -i "s/BRANCH_REPLACEME/$BRANCH/" version.h
+sed -i "s/ISTAG_REPLACEME/$ISTAG/" version.h
