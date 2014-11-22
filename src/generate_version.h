c23e7272ac2ebdd62a80aa6ac8cb95a1d8ae2039
Sergio Martins <sergio.martins@kdab.com>
2014-11-22 16:21:20 +0000

    Remove stale file

--- src/generate_version.h
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp version_template.h version.h
-LAST_SHA1=`git rev-parse HEAD`
-LAST_DATE=`git log -1 --format=%cd --date=short`
-sed -i "s/SHA1_REPLACEME/$LAST_SHA1/" version.h
-sed -i "s/DATE_REPLACEME/$LAST_DATE/" version.h
