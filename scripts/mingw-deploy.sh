6ba42674d4e58c1eeb417edd58335263e6d5d1c3
Sergio Martins <iamsergio@gmail.com>
2014-12-03 16:09:26 +0000

    Add script to make windows deployment

---
+++ scripts/mingw-deploy.sh
@@ -0,0 +1,32 @@
+QT=/data/extra2/installation/qt/x86_64-mingw-5.3/
+
+rm -rf flow &> /dev/null
+mkdir flow
+cd flow
+
+cp $QT/bin/Qt5Core.dll .
+cp $QT/bin/Qt5Gui.dll .
+cp $QT/bin/Qt5Qml.dll .
+cp $QT/bin/Qt5Quick.dll .
+cp $QT/bin/Qt5Widgets.dll .
+cp $QT/bin/Qt5Network.dll .
+
+cp /usr/i686-w64-mingw32/bin/libgcc_s_sjlj-1.dll .
+cp /usr/i686-w64-mingw32/bin/libstdc++-6.dll .
+cp /usr/i686-w64-mingw32/bin/libwinpthread-1.dll .
+cp /data/opt/mingw32/bin/opengl32.dll .
+
+
+rsync -av $QT/plugins/platforms/ platforms/
+rsync -av $QT/qml/QtQml/ QtQml/
+rsync -av $QT/qml/QtQuick/ QtQuick/
+rsync -av $QT/qml/QtQuick.2/ QtQuick.2/
+rsync -av $QT/qml/QtGraphicalEffects/ QtGraphicalEffects/
+
+cp ../flow.exe .
+mkdir plugins
+cp ../plugins/*.dll plugins
+
+cd ..
+
+zip -r flow.zip flow/
