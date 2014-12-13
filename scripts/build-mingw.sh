24b5b5d4e1402e63dacd39d57789e7f231263e6a
Sergio Martins <iamsergio@gmail.com>
2014-12-13 22:39:54 +0000

    no need to git clean when building

--- scripts/build-mingw.sh
+++ scripts/build-mingw.sh
@@ -1,7 +1,6 @@
 source /data/qt5-x86_64-mingw.source 5.4
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
-git clean -fdx
 mkdir build-mingw
 cd build-mingw
 qmake .. && make -j10 && sh ../scripts/mingw-deploy.sh
--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,7 +1,6 @@
 source /data/qt5-x86_64.source 5.3 static
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
-git clean -fdx
 mkdir build-osx
 cd build-osx
 qmake .. && make -j10 && sh ../scripts/osx-deploy.sh
