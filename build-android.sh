f95cd56fe7b4d17f89a6753bc49615583ff28d8e
Sergio Martins <iamsergio@gmail.com>
2015-01-05 00:48:29 +0000

    Android: Don't nuke the data files when installing apk

--- build-android.sh
+++ build-android.sh
@@ -9,4 +9,4 @@ qmake CONFIG+=release ../flow.pro
 make -j8
 make install INSTALL_ROOT=android-build
 
-androiddeployqt --input src/android-libflow.so-deployment-settings.json --output src/android-build/ --deployment bundled --ant /usr/bin/ant --android-platform android-19 --jdk /usr/lib/jvm/java-7-openjdk --install --device 058970b2f0df5762
+androiddeployqt --input src/android-libflow.so-deployment-settings.json --output src/android-build/ --deployment bundled --ant /usr/bin/ant --android-platform android-19 --jdk /usr/lib/jvm/java-7-openjdk --reinstall --device 058970b2f0df5762
