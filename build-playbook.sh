cb271a1880fb823870010934a3bf648230ef0643
Sergio Martins <iamsergio@gmail.com>
2015-04-03 22:35:02 +0100

    Remove playbook scripts, it's dead

--- build-playbook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir build_dir-playbook &> /dev/null
-cd build_dir-playbook
-qmake ../flow.pro
-make -j8
-
-# blackberry-nativepackager -devMode -package flow-pomodoro.bar bar-descriptor.xml
-# blackberry-deploy -installApp -device 192.168.1.143 -password kdab flow-pomodoro.bar
