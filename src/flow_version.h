77077e535b95f1bf6cedea7f24e0048d5adb5f6c
Sergio Martins <iamsergio@gmail.com>
2015-05-23 12:32:22 +0100

    Bump version to 1.0.2

--- src/flow_version.h
+++ src/flow_version.h
@@ -1,7 +1,7 @@
 #ifndef FLOW_VERSION_H_
 #define FLOW_VERSION_H_
 
-#define FLOW_VERSION v1.0.1
-#define FLOW_VERSION_DATE 15-03-2015
+#define FLOW_VERSION v1.0.2
+#define FLOW_VERSION_DATE 23-05-2015
 
 #endif
