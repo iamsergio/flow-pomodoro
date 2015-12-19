08594d99d71d6303f3103110773166687241138b
Sergio Martins <iamsergio@gmail.com>
2015-12-19 23:39:27 +0000

    Update my mingw script

--- scripts/build-mingw.sh
+++ scripts/build-mingw.sh
@@ -1,4 +1,4 @@
-source /data/qt5-x86_64-mingw.source 5.4
+source /data/qt5-x86_64-mingw.source 5.5
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 mkdir build-mingw
--- scripts/mingw-deploy.sh
+++ scripts/mingw-deploy.sh
@@ -1,4 +1,4 @@
-QT=/data/extra2/installation/qt/x86_64-mingw-5.4/
+QT=/data/installation/qt/x86_64-mingw-5.5/
 
 rm -rf flow &> /dev/null
 mkdir flow
