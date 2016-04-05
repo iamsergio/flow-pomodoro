5b08e1126237ae3ead876540ac2db8596c37fe7c
Sergio Martins <iamsergio@gmail.com>
2016-04-05 23:45:06 +0100

    Update copyright

--- tests/quick/testui.h
+++ tests/quick/testui.h
@@ -1,7 +1,7 @@
 /*
   This file is part of Flow.
 
-  Copyright (C) 2015 Sérgio Martins <iamsergio@gmail.com>
+  Copyright (C) 2015-2016 Sérgio Martins <iamsergio@gmail.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
