73dd08d08b398afcc02d1ca9d5f62647da8dc3c0
Sergio Martins <iamsergio@gmail.com>
2018-08-16 17:38:34 +0100

    Update copyright

--- src/taskfilterproxymodel.cpp
+++ src/taskfilterproxymodel.cpp
@@ -1,7 +1,7 @@
 /*
   This file is part of Flow.
 
-  Copyright (C) 2014 Sérgio Martins <iamsergio@gmail.com>
+  Copyright (C) 2014, 2018 Sérgio Martins <iamsergio@gmail.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
